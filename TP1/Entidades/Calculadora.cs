609bdbb51edcd120d52ddd9fdc3a5ab075c8598d
pabloebarragan <42048036+pabloebarragan@users.noreply.github.com>
2021-04-19 03:22:15 -0300

    Add files via upload

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+
+        /// <summary>
+        /// Realiza la operacion entre 2 numeros.
+        /// </summary>
+        /// <param name="num1">numero 1 </param>
+        /// <param name="num2">numero 2</param>
+        /// <param name="operador"></param>
+        /// <returns>retorna el resultado de la operacion</returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double retorno = 0;
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    retorno = num1 + num2;
+                    break;
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+                case "/":
+                    retorno = num1 / num2;
+                    break;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Valida que el operador recibido sea +, -, / o *.
+        /// </summary>
+        /// <param name="operador">operador a validar</param>
+        /// <returns>Retorna operador si es valido. Caso contrario retorna +.</returns>
+        private static string ValidarOperador(string operador)
+        {
+            string retorno = "+";
+            if (operador == "+" || operador == "-" || operador == "*" || operador == "/")
+            {
+                retorno = operador;
+            }
+            return retorno;
+        }
+    }
+
+
+
+}
